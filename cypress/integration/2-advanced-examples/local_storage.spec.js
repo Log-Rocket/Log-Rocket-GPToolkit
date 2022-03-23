c76da7152f5b829ddbe012bd3b8849a7e187401e
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-03-23 06:45:13 +0000

    fixed html

--- cypress/integration/2-advanced-examples/local_storage.spec.js
+++ cypress/integration/2-advanced-examples/local_storage.spec.js
@@ -1,1 +1,7 @@
-/// <referenc
+/// <reference types="cypress" />
+
+context('Local Storage', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/commands/local-storage')
+  })
+  /
