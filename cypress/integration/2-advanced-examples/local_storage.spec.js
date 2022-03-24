1bf7391ef51cf55b0e9b828ade6a5ee5b3d7bbfb
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-03-24 15:48:03 +0000

    Formating

--- cypress/integration/2-advanced-examples/local_storage.spec.js
+++ cypress/integration/2-advanced-examples/local_storage.spec.js
@@ -4,4 +4,6 @@ context('Local Storage', () => {
   beforeEach(() => {
     cy.visit('https://example.cypress.io/commands/local-storage')
   })
-  /
+  // Although local storage is automatically cleared
+  // in between tests to maintain a clean state
+  // sometimes we need to clear the local storage manually
