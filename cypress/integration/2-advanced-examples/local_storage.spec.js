63b082526a6f54e43099067700e9626fb1c3e26b
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-03-29 11:38:14 +0000

    Update deployment script

--- cypress/integration/2-advanced-examples/local_storage.spec.js
+++ cypress/integration/2-advanced-examples/local_storage.spec.js
@@ -7,3 +7,8 @@ context('Local Storage', () => {
   // Although local storage is automatically cleared
   // in between tests to maintain a clean state
   // sometimes we need to clear the local storage manually
+
+  it('cy.clearLocalStorage() - clear all data in local storage', () => {
+    // https://on.cypress.io/clearlocalstorage
+    cy.get('.ls-btn').click().should(() => {
+      expec
