b834b8204c16c5eaf64406791b2d3da20a4108de
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-04-03 22:13:36 +0000

    add config example

--- cypress/integration/2-advanced-examples/local_storage.spec.js
+++ cypress/integration/2-advanced-examples/local_storage.spec.js
@@ -23,4 +23,7 @@ context('Local Storage', () => {
       expect(ls.getItem('prop3')).to.be.null
     })
 
-    cy.get('.ls-bt
+    cy.get('.ls-btn').click().should(() => {
+      expect(localStorage.getItem('prop1')).to.eq('red')
+      expect(localStorage.getItem('prop2')).to.eq('blue')
+   
