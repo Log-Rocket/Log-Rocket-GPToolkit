771ea292c8737ac011216c31125f3f1e6d154e88
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-05-07 17:18:35 +0000

    Add dummy test

--- cypress/integration/2-advanced-examples/misc.spec.js
+++ cypress/integration/2-advanced-examples/misc.spec.js
@@ -5,4 +5,8 @@ context('Misc', () => {
     cy.visit('https://example.cypress.io/commands/misc')
   })
 
-  it(
+  it('.end() - end the command chain', () => {
+    // https://on.cypress.io/end
+
+    // cy.end is useful when you want to end a chain of commands
+    // and force Cyp
