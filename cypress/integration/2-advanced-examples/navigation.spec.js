a5e437e80f67ca3adbb6a8cbeb11958ce561bbd1
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-07-31 11:15:57 +0000

    USB: option: add more YUGA device ids

--- cypress/integration/2-advanced-examples/navigation.spec.js
+++ cypress/integration/2-advanced-examples/navigation.spec.js
@@ -13,4 +13,11 @@ context('Navigation', () => {
     cy.location('pathname').should('include', 'navigation')
 
     cy.go('back')
-    cy.location('pathname').should('not.include', 'navig
+    cy.location('pathname').should('not.include', 'navigation')
+
+    cy.go('forward')
+    cy.location('pathname').should('include', 'navigation')
+
+    // clicking back
+    cy.go(-1)
+    cy.location('pathname').should('not.include', 'nav
