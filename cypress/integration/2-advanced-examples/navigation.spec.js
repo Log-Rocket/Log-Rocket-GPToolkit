4503a1fc7d6272e8d9c8deef10700df4edc8ca9c
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-08-17 09:25:59 +0000

    Add CNAME file.

--- cypress/integration/2-advanced-examples/navigation.spec.js
+++ cypress/integration/2-advanced-examples/navigation.spec.js
@@ -44,4 +44,8 @@ context('Navigation', () => {
     cy.visit('https://example.cypress.io/commands/navigation', {
       timeout: 50000, // increase total time for the visit to resolve
       onBeforeLoad (contentWindow) {
-        // contentWindow is the remote pa
+        // contentWindow is the remote page's window object
+        expect(typeof contentWindow === 'object').to.be.true
+      },
+      onLoad (contentWindow) {
+        // contentWindow is the remote page's window
