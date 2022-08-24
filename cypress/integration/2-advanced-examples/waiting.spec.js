dd1cd9d394d0f5f070425330e17569b8366f48e9
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-08-24 22:20:59 +0000

    Update android_parcel.txt

--- cypress/integration/2-advanced-examples/waiting.spec.js
+++ cypress/integration/2-advanced-examples/waiting.spec.js
@@ -11,4 +11,10 @@ context('Waiting', () => {
   it('cy.wait() - wait for a specific amount of time', () => {
     cy.get('.wait-input1').type('Wait 1000ms after typing')
     cy.wait(1000)
-    cy.get('.wait-input2').type('Wait 1000ms 
+    cy.get('.wait-input2').type('Wait 1000ms after typing')
+    cy.wait(1000)
+    cy.get('.wait-input3').type('Wait 1000ms after typing')
+    cy.wait(1000)
+  })
+
+  it('cy.wait() - wait for a specific rou
