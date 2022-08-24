7f650d05491cd425fb2336e46d779de035f5c82c
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-08-24 06:09:11 +0000

    added helper

--- cypress/integration/2-advanced-examples/waiting.spec.js
+++ cypress/integration/2-advanced-examples/waiting.spec.js
@@ -2,4 +2,10 @@
 
 context('Waiting', () => {
   beforeEach(() => {
-    cy.visit('https://examp
+    cy.visit('https://example.cypress.io/commands/waiting')
+  })
+  // BE CAREFUL of adding unnecessary wait times.
+  // https://on.cypress.io/best-practices#Unnecessary-Waiting
+
+  // https://on.cypress.io/wait
+  it('cy.wait
