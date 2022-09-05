9355297412436cb54d38016ea9f9cb310b85e3a9
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-09-05 10:51:53 +0000

    removing warnings

--- cypress/integration/GPToolkit-tests/full_suite.js
+++ cypress/integration/GPToolkit-tests/full_suite.js
@@ -3,4 +3,8 @@
 // Welcome to Cypress!
 //
 // This spec file contains a variety of sample tests
-// f
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// wha
