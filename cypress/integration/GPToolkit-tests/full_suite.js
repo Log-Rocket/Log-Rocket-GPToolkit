c2c5583ce967a89b418fa5396c6d257dbe08198f
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-09-05 20:07:40 +0000

    staging: vt6656: use free_netdev instead of kfree

--- cypress/integration/GPToolkit-tests/full_suite.js
+++ cypress/integration/GPToolkit-tests/full_suite.js
@@ -17,4 +17,11 @@ describe('basicTestSuite brainstorm-call-&-response', () => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('localhost:3000')
+  })
+
+  it('click brainstorm button', () => {
+
+    // set up listener for xhr post request
+    cy.intercept
