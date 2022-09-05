12f1636bcc8da1bf9610fe5d0e873f812101aa91
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-09-05 08:46:09 +0000

    Code beautification.

--- cypress/integration/2-advanced-examples/waiting.spec.js
+++ cypress/integration/2-advanced-examples/waiting.spec.js
@@ -22,4 +22,8 @@ context('Waiting', () => {
     cy.intercept('GET', '**/comments/*').as('getComment')
 
     // we have code that gets a comment when
-    // th
+    // the button is clicked in scripts.js
+    cy.get('.network-btn').click()
+
+    // wait for GET comments/1
+    cy.wait('@getComment').its('response.statusCode').should('be.oneOf', [
