568152203d7ea1d4707b5f054cec38f1f8d8da2e
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-09-30 14:03:05 +0000

    Fix broken image

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,2 +1,5 @@
 /// <reference types="cypress" />
-// 
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location
