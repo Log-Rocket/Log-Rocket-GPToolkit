ca34001b26aea29200f34cd623b1e042964d6fc5
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-10-11 21:09:44 +0000

    msm_cpufreq_limit: add GPL V2 licensing to access to GPL symbols

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -18,4 +18,8 @@
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { .
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
