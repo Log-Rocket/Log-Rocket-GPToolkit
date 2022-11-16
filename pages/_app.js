c77caefc86eb690e587a7b99244afcdae43864d1
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-11-16 18:03:56 +0000

    fix some minor bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,8 @@
 import Head from 'next/head';
 import { useState } from 'react';
-import { MantineProvider, TypographyStylesProvider, ColorSchemeProvider } from '@man
+import { MantineProvider, TypographyStylesProvider, ColorSchemeProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+
+export default function App(props) {
+  
+  const { Component, pageProps } = 
