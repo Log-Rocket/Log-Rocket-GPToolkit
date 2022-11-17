cadea863401de8b379b952f353604a4bdaeb4bab
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-11-17 05:57:12 +0000

    1.7.7

--- pages/_app.js
+++ pages/_app.js
@@ -9,4 +9,10 @@ export default function App(props) {
 
   const [colorScheme, setColorScheme] = useState('light');
   const toggleColorScheme = (value) =>
-    setColorScheme(value || (colo
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale
