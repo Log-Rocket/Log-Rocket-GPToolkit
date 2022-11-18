01dd25e2d9a4780ad18b69e1b40669160c1b4271
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-11-18 22:02:55 +0000

    Update aLSTM.lua

--- pages/api/handleRequest.js
+++ pages/api/handleRequest.js
@@ -1,3 +1,6 @@
 const OpenAI = require('openai-api')
 
-async function s
+async function sendQuery(operation, prompt, key) {
+
+  //Create a new OpenAi instance with provided key but default to env for development
+  const openai = new OpenAI(key ? key : pro
