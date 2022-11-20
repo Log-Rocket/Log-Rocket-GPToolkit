2c2fbe9749206490befffb310bb0df559f73043b
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-11-20 19:07:42 +0000

    Course Menu block.

--- pages/api/handleRequest.js
+++ pages/api/handleRequest.js
@@ -3,4 +3,10 @@ const OpenAI = require('openai-api')
 async function sendQuery(operation, prompt, key) {
 
   //Create a new OpenAi instance with provided key but default to env for development
-  const openai = new OpenAI(key ? key : pro
+  const openai = new OpenAI(key ? key : process.env.OPENAI_API_KEY);
+  var gptResponse;
+
+  switch(operation) {
+    
+    // [Brainstorm] Model for generating an elevator pitch or new business idea
+   
