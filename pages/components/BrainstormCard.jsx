91f600aae251d5258634d0619d31a194f8d5abb6
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-12-08 20:05:57 +0000

    version 0.8.3

--- pages/components/BrainstormCard.jsx
+++ pages/components/BrainstormCard.jsx
@@ -1,4 +1,7 @@
 import { Center, Paper, Select, Text, Button, Divider, Skeleton } from '@mantine/core';
 import { useState } from 'react';
 
-const sectors = 
+const sectors = [
+    {value: 'Aerospace and aviation ', label: 'Aerospace and aviation '},
+    {value: 'Agriculture ', label: 'Agriculture '},
+    {value: 'Appliances ', label: 'Appli
