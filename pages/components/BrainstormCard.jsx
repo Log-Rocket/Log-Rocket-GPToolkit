4bbe737726fc280faeb23b603f0fb276979b80aa
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2022-12-10 20:54:09 +0000

    Adding license.

--- pages/components/BrainstormCard.jsx
+++ pages/components/BrainstormCard.jsx
@@ -4,4 +4,7 @@ import { useState } from 'react';
 const sectors = [
     {value: 'Aerospace and aviation ', label: 'Aerospace and aviation '},
     {value: 'Agriculture ', label: 'Agriculture '},
-    {value: 'Appliances ', label: 'Appli
+    {value: 'Appliances ', label: 'Appliances '},
+    {value: 'Automotive ', label: 'Automotive '},
+    {value: 'Banking and financial services', label: 'Banking and financial services'},
+    {value: 'B
