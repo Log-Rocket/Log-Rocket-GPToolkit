0ae171ac6f646f47e4152453aa16379c11b5b762
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2023-01-12 21:02:17 +0000

    Control

--- pages/components/IdeaToCodeCard.jsx
+++ pages/components/IdeaToCodeCard.jsx
@@ -1,2 +1,8 @@
 import { Center, Select, Text, Button, Divider, Skeleton, Grid, Textarea, useMantineColorScheme } from '@mantine/core';
-import { Prism } from '@mantin
+import { Prism } from '@mantine/prism';
+import { GiDiamondHard } from 'react-icons/gi'
+import { useState } from 'react';
+
+const programmingLanguages = [
+  {value: 'Bash', label: 'Bash'},
+  {value: 'C++', labe
