6585f1c18a890f9d35c1962959ae03ed9e6c1a44
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2023-02-17 20:19:36 +0000

    Fix function name.

--- pages/components/IdeaToCodeCard.jsx
+++ pages/components/IdeaToCodeCard.jsx
@@ -130,4 +130,6 @@ export default function IdeaToCodeCard({sendPrompt}){
                         creatable
                         clearable
                         getCreateLabel={(query) => `+ Create ${query}`}
-                        onCreat
+                        onCreate={(query) => setData((current) => [...current, query])}
+                        style={{borderBottom: '1px solid #e0e0e0', width: '90%'}}
+                        r
