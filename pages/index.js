1cdd5982f750a6eb84ac2ce12937b50b7e4cc408
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2023-02-22 05:35:36 +0000

    fixed syntax errors

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,3 @@
-import Head from 'next/hea
+import Head from 'next/head'
+import { showNotification } from '@mantine/notifications';
+import { Center, Button, Paper, Text, Tabs, Alert, ActionIcon, Modal, PasswordInput, useMantin
