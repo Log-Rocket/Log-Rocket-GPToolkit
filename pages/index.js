e54ddf6d2d6f264ea94f5d8641505c8e3848ff63
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2023-02-22 20:38:14 +0000

    Pic

--- pages/index.js
+++ pages/index.js
@@ -5,4 +5,8 @@ import { RiMoonClearFill, RiSunFill } from 'react-icons/ri'
 import { AiOutlineKey } from 'react-icons/ai'
 import { SiBuymeacoffee } from 'react-icons/si'
 import { GiToolbox, GiBrainstorm, GiComputing, GiBriefcase, GiBookCover, GiPalette } from 'react-icons/gi'
-import { useState } from
+import { useState } from 'react';
+import { FaHeartBroken } from 'react-icons/fa';
+import BrainstormCard from './components/BrainstormCard';
+import PromptCard from './components/PromptCard';
+import Idea
