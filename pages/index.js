18a4dd8943532275fc7ef9c276878f53570206e1
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2023-02-22 13:28:17 +0000

    Added vimrc

--- pages/index.js
+++ pages/index.js
@@ -3,4 +3,6 @@ import { showNotification } from '@mantine/notifications';
 import { Center, Button, Paper, Text, Tabs, Alert, ActionIcon, Modal, PasswordInput, useMantineColorScheme } from '@mantine/core';
 import { RiMoonClearFill, RiSunFill } from 'react-icons/ri'
 import { AiOutlineKey } from 'react-icons/ai'
-import { SiBuy
+import { SiBuymeacoffee } from 'react-icons/si'
+import { GiToolbox, GiBrainstorm, GiComputing, GiBriefcase, GiBookCover, GiPalette } from 'react-icons/gi'
+import { useState } from
