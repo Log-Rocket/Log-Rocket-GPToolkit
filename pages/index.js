379b136c9616ad76b8ec23a79586db31a8ee433a
Log-Rocket <110128828+Log-Rocket@users.noreply.github.com>
2023-02-22 06:32:51 +0000

    Updated Polish translation.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
 import Head from 'next/head'
 import { showNotification } from '@mantine/notifications';
-import { Center, Button, Paper, Text, Tabs, Alert, ActionIcon, Modal, PasswordInput, useMantin
+import { Center, Button, Paper, Text, Tabs, Alert, ActionIcon, Modal, PasswordInput, useMantineColorScheme } from '@mantine/core';
+import { RiMoonClearFill, RiSunFill } from 'react-icons/ri'
+import { AiOutlineKey } from 'react-icons/ai'
+import { SiBuy
